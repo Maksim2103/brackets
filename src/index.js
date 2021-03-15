0b5ac1b03432a2b84dc197584286803e7a161b75
Maksim <maksim2103@yandex.ru>
2021-03-15 09:56:12 +0300

    feat: add desicion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    // your solution
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] == "(") {
+            count = count + 1;
+        }
+        if (str[i] == ")") {
+            count = count - 1;
+        }
+        if (str[i] == "{") {
+            count = count + 100;
+        }
+        if (str[i] == "}") {
+            count = count - 100;
+        }
+        if (str[i] == "[") {
+            count = count + 1000;
+        }
+        if (str[i] == "]") {
+            count = count - 1000;
+        }
+        if (count < 0) {
+            break;
+        }
+    }
+    return count != 0 ? false : true;
+};
